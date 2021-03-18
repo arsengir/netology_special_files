47eaa8e255dba2a4185532ff173e76ea5dcdea7d
Arsen Girinov <amateurlib@yandex.ru>
2021-03-18 23:22:19 +0300

    task2

--- src/main/java/com/github/arsengir/Main.java
+++ src/main/java/com/github/arsengir/Main.java
@@ -7,31 +7,56 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         csvToJson();
+        xmlToJson();
+    }
+
+    private static void xmlToJson() {
+        String fileXmlIn = "data.xml";
+        String fileJsonOut = "data2.json";
+        List<Employee> list = null;
+        try {
+            list = parseXML(fileXmlIn);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+
+        String json = listToJson(list);
+        writeString(fileJsonOut, json);
     }
 
     private static void csvToJson() {
-        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileCsvIn = "data.csv";
         String fileJsonOut = "data.json";
 
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         List<Employee> list = parseCSV(columnMapping, fileCsvIn);
         String json = listToJson(list);
         writeString(fileJsonOut, json);
     }
 
     private static void writeString(String fileName, String text) {
-        try(FileWriter fileWriter = new FileWriter(fileName)){
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
             fileWriter.write(text);
             fileWriter.flush();
         } catch (IOException e) {
@@ -51,6 +76,37 @@ public class Main {
             staff = csv.parse();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        return staff;
+    }
+
+    private static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> staff = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File(fileName));
+
+        Node root = document.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node employee = nodeList.item(i);
+            if (Node.ELEMENT_NODE == employee.getNodeType()) {
+                NodeList employeeFields = employee.getChildNodes();
+                HashMap<String, String> employeeFieldMap = new HashMap<>();
+                for (int j = 0; j < employeeFields.getLength(); j++) {
+                    Node employeeField = employeeFields.item(j);
+                    if (Node.ELEMENT_NODE == employeeField.getNodeType()) {
+                        Element element = (Element) employeeField;
+                        employeeFieldMap.put(element.getNodeName(), element.getTextContent());
+                    }
+                }
+                staff.add(new Employee(Long.parseLong(employeeFieldMap.get("id")),
+                        employeeFieldMap.get("firstName"),
+                        employeeFieldMap.get("lastName"),
+                        employeeFieldMap.get("country"),
+                        Integer.parseInt(employeeFieldMap.get("age"))));
+            }
         }
         return staff;
     }
